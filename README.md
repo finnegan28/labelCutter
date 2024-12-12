0f76e88d5af48d3f6fefb2719f08913bef09afa6
Neil Finnegan <neil.nitro@gmail.com>
2024-12-12 15:49:27 +0000

    Fully working python implementation

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # labelCutter
-A tool to resize Royal Mail International Post labels for eBay
+A tool to resize Royal Mail International Postage labels generated through eBay.
+
+## Why is this needed?
+I sell a lot of items on eBay and I use the integrated postage label purchase system.
+
+I eventually bought a label printer which prints out postage labels using the A6 page size.
+
+On eBay you get the option of regular A4 size labels or A6 for label printers.
+
+This worked great for a while, that was until some genius decided to output all international labels as an A4 page, regardless of selecting the A6 size option.
+
+This means that you can't print directly to your label printer.
+
+Like many others, I would have to manually fix all the labels and feed them to my printer. This is time consuming and if you have to do something more than once, then you might as well code something to take care of it.
+
+
+### Usage
+python3 labelCutter.py label.pdf
+
+This will output all labels in order to a PDF.
---
+++ labelCutter.py
@@ -0,0 +1,73 @@
+import argparse
+import os
+from fpdf import FPDF
+import fitz
+from PIL import Image
+from datetime import datetime
+
+CM_TO_POINTS = 28.3465
+datestamp = datetime.now().strftime("%d-%m-%y")
+
+region1_cm = (0.3, 0.0, 10.3, 14.15)
+region2_cm = (10.7, 0.0, 20.75, 14.15)
+
+region1 = fitz.Rect(*[coord * CM_TO_POINTS for coord in region1_cm])
+region2 = fitz.Rect(*[coord * CM_TO_POINTS for coord in region2_cm])
+
+parser = argparse.ArgumentParser(description='Process a PDF file to extract labels.')
+parser.add_argument('input_file', type=str, help='The input PDF file')
+args = parser.parse_args()
+
+if not os.path.isfile(args.input_file):
+    print(f"Error: The file {args.input_file} does not exist.")
+    exit(1)
+
+if not args.input_file.lower().endswith('.pdf'):
+    print("Error: The input file must be a PDF.")
+    exit(1)
+
+pdf_document = fitz.open(args.input_file)
+
+if len(pdf_document) == 0:
+    print("The PDF document is empty. Exiting.")
+    exit()
+
+a6_width_points = 1240 * 72 / 300
+a6_height_points = 1748 * 72 / 300
+
+output_pdf = FPDF(orientation='P', unit='pt', format=(a6_width_points, a6_height_points))
+
+for page_number in range(len(pdf_document)):
+    
+    page = pdf_document[page_number]
+
+    if page.rect.width < 11 * CM_TO_POINTS:
+        print(f"Skipping page {page_number + 1} as it is already A6 size.")
+        continue
+
+    pix1 = page.get_pixmap(clip=region1, dpi=400)
+    pix2 = page.get_pixmap(clip=region2, dpi=400)
+
+    img1 = Image.frombytes("RGB", [pix1.width, pix1.height], pix1.samples)
+    img2 = Image.frombytes("RGB", [pix2.width, pix2.height], pix2.samples)
+
+    a6_size = (1240, 1748)
+    img1 = img1.resize(a6_size, Image.LANCZOS)
+    img2 = img2.resize(a6_size, Image.LANCZOS)
+
+    img1_path = f"{page_number + 1}_Label.png"
+    img2_path = f"{page_number + 1}_CN22.png"
+    img1.save(img1_path)
+    img2.save(img2_path)
+
+    output_pdf.add_page()
+    output_pdf.image(img1_path, 0, 0, a6_width_points, a6_height_points)
+    output_pdf.add_page()
+    output_pdf.image(img2_path, 0, 0, a6_width_points, a6_height_points)
+
+    os.remove(img1_path)
+    os.remove(img2_path)
+
+output_pdf.output(f"ebay labels - {datestamp}.pdf")
+
+print("ebay labels successfully converted to A6")
